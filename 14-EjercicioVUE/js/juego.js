8ba84638dd4bde758bbcc278e9b06eda69512cc9
marbrian <martinezbrian.exe@gmail.com>
2022-04-19 23:22:55 -0300

    Update - Actualizo y mejoro ejercicio 14

--- 14-EjercicioVUE/js/juego.js
+++ 14-EjercicioVUE/js/juego.js
@@ -5,10 +5,9 @@ const juego = new Vue({
         personas: [],
         resultado: [],
         orden: [],
+        deshabilitado: [],
         mensaje: '',
         estado: 'Empieza',
-        deshabilitado: [,true,true,true,true,true,true,true,true,true,true],
-        verificar: function(array1,array2) { return array1.toString() === array2.toString(); },
         gana: new Audio('audios/gana.mp3'),
         intenta: new Audio('audios/intento.mp3'),
         pierde: new Audio('audios/pierde.mp3'),
@@ -18,18 +17,31 @@ const juego = new Vue({
 
     mounted(){
         axios.get(this.url)
-            .then(respuesta => this.personas = respuesta.data)
+            .then(respuesta => {
+                res = respuesta.data;
+                for (let i = 0; i < res.length; i++) {
+                    this.personas.push(res[i].username);
+                    this.deshabilitado[i] = true;
+                }
+                this.orden = this.personas.slice();
+                this.orden.sort();
+            })
             .catch(error => console.log(error));
     },
 
     methods:{
-        pasar(nombre,id){
+        pasar(nombre){
             //Pasar cada nombre al nuevo array y deshabilitar el botón correspondiente
             if (this.resultado.length<10) {
                 this.resultado.push(nombre);
-                this.deshabilitado[id]=false;
+                for (let i = 0; i < this.personas.length; i++) {
+                    if (this.personas[i]===nombre) {
+                        this.deshabilitado[i] = false;
+                    }
+                }
             } 
         },
+
         nuevoIntento(){
             //Devuelvo los valores originales a las variables
             this.resultado = [];
@@ -38,44 +50,48 @@ const juego = new Vue({
             this.estado = 'Empieza';
             this.fin = false;
             this.intenta.play();
-            for (let i = 0; i < 11; i++) {
+            for (let i = 0; i < this.deshabilitado.length; i++) {
                 this.deshabilitado[i] = true;
             }
         },
+
         reiniciar(){
             window.location.reload();
-        }
+        },
+
+        verificar(array1,array2) {
+            return array1.toString() === array2.toString(); 
+        },
     },
 
     watch:{
         resultado(nuevo, antiguo){
             // Si el rango del array resultado es menor a 10, que me muestre el mensaje de continuar
-            if((nuevo.length > 0) && (nuevo.length <= 9)){
+            if((nuevo.length > 0) && (nuevo.length < this.personas.length)){
                 this.estado = "Continua";
                 return false;
             }else if(nuevo.length == 0){
                 this.estado = "Empieza";
                 return false;
+            }else{
+                this.estado = "Terminaste";
+                this.orden = nuevo.slice();
+                this.orden.sort();
+                this.fin = true;
             }
-            this.estado = "Terminaste";
-            this.orden = nuevo.slice();
-            this.orden.sort();
-            this.fin = true;
-        },
-        fin(t,f){
+
             //Si fin es TRUE, comparar los arrays y mostrar el mensaje correspondiente
-            if(t && this.verificar(this.resultado, this.orden)){
+            if(this.fin && this.verificar(this.resultado, this.orden)){
                 this.mensaje = 'Está ordenado. ÉXITO';
                 this.gana.play();
-            }else if(t && !this.verificar(this.resultado, this.orden)){
+            }else if(this.fin && !this.verificar(this.resultado, this.orden)){
                 this.mensaje = 'No está ordenado. Vuelve a intentarlo.';
             }
             else{
                 this.mensaje = '';
             }
-        },
-        mensaje(a,d){
-            if((a==='No está ordenado. Vuelve a intentarlo.') && (this.intentos === 0)){
+
+            if((this.mensaje==='No está ordenado. Vuelve a intentarlo.') && (this.intentos === 0)){
                 alert('Te has quedado sin intentos!');
                 this.pierde.play();
             }
